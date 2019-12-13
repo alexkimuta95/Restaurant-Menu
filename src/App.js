d85941b97d20e9088ed75e50a1574d8294fa5154
Alex Kimutai <billroberts2001@gmail.com>
2019-12-13 09:10:50 +0300

    Retaurant menu in react js using hard coded menu items

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import MenuItemGroup from './components/MenuItemGroup';
+import menuList from './data/menuList';
+import Header from './components/layout/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ class App extends Component {
+
+  state = {
+    menuList,
+};
+getStyle = () => {
+ return{
+  margin: '20',
+  background:'#f4f4f4',
+  padding:'10px',
+  borderBottom:'1px #ccc dotted'
+ }
+
 }
 
-export default App;
+onCheckMenuItem = (path, type) => (e) => {
+    let {menuList} = this.state;
+
+    let traversalMenuList = menuList;
+
+    let nameVal = e.target.value;
+    let isChecked = e.target.checked;
+
+    let selectedMenuItems = path.split("|");
+
+    if (type === "root") {
+        traversalMenuList.map((menuItem) => {
+            if (menuItem["name"] === nameVal)  {
+                if (type === "root") {
+                    if (menuItem.hasOwnProperty("isMenuSelected")) {
+                        menuItem["isMenuSelected"] = isChecked;
+                    } else {
+                        menuItem["isMenuSelected"] = isChecked;
+                    }
+                }
+            }
+        });
+    }
+
+
+    if (type === "choices") {
+        traversalMenuList.map((menuItem) => {
+            if (menuItem["name"] === selectedMenuItems[0].trim()) {
+                menuItem.choices.map((choice) => {
+                    if (choice.name === selectedMenuItems[1].trim()) {
+                        if (menuItem.hasOwnProperty("isChoiceSelected")) {
+                            menuItem["isChoiceSelected"] = isChecked;
+                        } else {
+                            menuItem["isChoiceSelected"] = isChecked;
+                        }
+                    }
+                })
+            }
+        });
+    }
+
+
+    if (type === "related") {
+        traversalMenuList.map((menuItem) => {
+            if (menuItem["name"] === selectedMenuItems[0].trim()) {
+                menuItem.related.map((relatedItem) => {
+                    if (relatedItem.name === selectedMenuItems[1].trim()) {
+                        if (menuItem.hasOwnProperty("isRelatedItemSelected")) {
+                            relatedItem["isRelatedItemSelected"] = isChecked;
+                        } else {
+                            relatedItem["isRelatedItemSelected"] = isChecked;
+                        }
+                    }
+                })
+            }
+        });
+    }
+
+    this.setState({
+        ...this.state,
+        menuList: traversalMenuList
+    });
+
+
+};
+
+render() {
+    const {menuList} = this.state;
+
+    return (
+        <div className="Menu" style={this.getStyle()}>
+          <Header />
+            <h1>Restaurant Menu</h1>
+
+            {
+                menuList !== undefined ?
+                    menuList.map((menuItem, menuItemKey) => (
+                        <MenuItemGroup
+                            className={"menu-item-group"}
+                            key={menuItemKey}
+                            name={menuItem.name}
+                            choices={menuItem.hasOwnProperty("choices") ? menuItem.choices : []}
+                            related={menuItem.hasOwnProperty("related") ? menuItem.related : []}
+                            level={1}
+                            path={menuItem.name}
+                            type={'root'}
+                            menuItemChecked={menuItem.hasOwnProperty("isMenuSelected") ? menuItem.isMenuSelected : false}
+                            choiceChecked={menuItem.hasOwnProperty("isChoiceSelected") ? menuItem.isChoiceSelected : false}
+                            onCheckMenuItem={this.onCheckMenuItem}
+                        />
+                    ))
+                    : null
+            }
+        </div>
+    );
+}
+}
+
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import Enzyme, {shallow} from "enzyme";
+import Menu from './App';
+import Adapter from "enzyme-adapter-react-16"
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new Adapter()});
+
+
+describe("Menu component", () => {
+    // Test if the component renders
+    test("renders", () => {
+        const wrapper = shallow(<Menu/>);
+
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    // Test to see if the app doesnt break when menuList = undefined
+    test("doesnt break without menu data", () => {
+        const wrapper = shallow(<Menu/>);
+
+        wrapper.setState({menuList: undefined});
+
+        expect(wrapper.find(".menu-list-group")).toHaveLength(0);
+
+    });
+
+    // Test to see if the app doesnt break when menuList = []
+    test("doesnt break with an empty array of menu data", () => {
+        const wrapper = shallow(<Menu/>);
+
+        wrapper.setState({menuList: []});
+
+        expect(wrapper.find(".menu-list-group")).toHaveLength(0);
+
+    });
 });
